be40918a4be843dda8503d4dc2193d926b76c509
Rachel McGrath <rachelmcgrath1994@gmail.com>
2020-12-01 13:47:42 -0600

    Initial commit

---
+++ coordinates.js
@@ -0,0 +1,9 @@
+import axios from 'axios';
+//EXAMPLE My House: LAT: 30.400523 LONG:-97.699108
+const Url='https://data.austintexas.gov/resource/fmm2-ytyt.json';
+
+function transformPotholeData(data) {
+    return undefined;
+}
+
+axios.get(Url);
